0fabcd126615b113c7fbf7cb0dfe4bdbc391814a
Swarchal <S.Warchal@sms.ed.ac.uk>
2016-02-05 16:39:05 +0000

    better name and make into function

--- R/x_axis_v.R
+++ R/x_axis_v.R
@@ -2,4 +2,4 @@
 #'
 #' Theme for ggplot to rotate x-axis labels and pad them
 
-x_axis_v <- theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.25))
+xlab_rotate <- function() theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.25))
