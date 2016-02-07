828d7fc82066fce35fd656ac40141122a343bdc2
Swarchal <S.Warchal@sms.ed.ac.uk>
2016-02-07 19:17:08 +0000

    add is_integer

---
+++ R/is_integer.R
@@ -0,0 +1,20 @@
+#' What is.integer() should be
+#'
+#' Determines if a number is an integer.
+#'
+#' @param x number of vector of numbers
+#' @return boolean
+#' @export
+
+is_integer <- function(x){
+  
+  if(length(x) == 1){
+  if (x %% 1 == 0){
+    return(TRUE)
+  } else return(FALSE)
+  }
+  
+  if (length(x) > 1){
+    mapply(is_integer, x)
+  }
+}
