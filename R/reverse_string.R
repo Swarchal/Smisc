d9d124c3d0b0878c4759666764429241806a5267
Swarchal <S.Warchal@sms.ed.ac.uk>
2016-02-09 17:55:46 +0000

    add reverse_str

---
+++ R/reverse_string.R
@@ -0,0 +1,13 @@
+#' Reverse string
+#'
+#' As reverse("string") doesn't work, here's the function that does
+#'
+#' @param s string
+#' @return string. reverse of s
+#' @export
+#' @examples
+#' "reverse_str("string")
+
+reverse_str <- function(s){
+	paste(rev(strsplit(s,'')[[1]]),collapse='')
+}
