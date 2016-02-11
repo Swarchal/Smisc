aeb29c10422479a8ac3d7f995fb2aa6d98b28eb9
Swarchal <S.Warchal@sms.ed.ac.uk>
2016-02-11 15:41:19 +0000

    export to namespace

--- R/x_axis_v.R
+++ R/x_axis_v.R
@@ -1,5 +1,6 @@
 #' Vertical x-axis labels
 #'
 #' Theme for ggplot to rotate and pad x-axis labels
+#' @export
 
 xlab_rotate <- function() theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.25))
