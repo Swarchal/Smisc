d8cc55fffcb96cb776cdc3588a65882e7aefe1c7
Swarchal <S.Warchal@sms.ed.ac.uk>
2016-03-24 22:23:10 +0000

    test

--- tests/testthat/test-list_to_dataframe.R
+++ tests/testthat/test-list_to_dataframe.R
@@ -2,11 +2,12 @@ context("list_to_dataframe")
 
 # example data
 split_iris <- split(iris, iris$Species)
-list_out <- sapply(split_iris, function(x) {x["Species"] <- NULL; x})
+list_out <- lapply(split_iris, function(x) {x["Species"] <- NULL; x})
 out <- list_to_dataframe(list_out, col_name = "Species")
 
 test_that("returns errors when expected",{
     expect_error(list_to_dataframe(iris, col_name = "Species"))
     expect_is(out, 'data.frame')
-    expect_true(out == iris)
+    expect_equal(colnames(out), colnames(iris))
+    expect_equal(nrow(out), nrow(iris))
 })
