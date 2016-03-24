6ae558a3284a1fe641e3f5f6d6adf528bc86b9cf
Swarchal <S.Warchal@sms.ed.ac.uk>
2016-03-24 19:12:42 +0000

    fix tests

--- tests/testthat/test-cv.R
+++ tests/testthat/test-cv.R
@@ -1,6 +1,6 @@
 context("cv")
 
 test_that("returns expected values",{
-    expect_equal(cv(c(1,1,1,1,1,1)), O, tolerance = 1e-5)
-    expect_true(cv(rnorm(100, 100, 100)), > 50)
+    expect_equal(cv(c(1,1,1,1,1,1)), 0, tolerance = 1e-5)
+    expect_true(cv(rnorm(100, 100, 100)) > 50)
 })
--- tests/testthat/test-scale01.R
+++ tests/testthat/test-scale01.R
@@ -1,8 +1,6 @@
 context("scale01")
 
 test_that("returns expected values",{
-    expect_equal(min(scale01(rnorm(100)), 0, tolerance = 1e-5))
+    expect_equal(min(scale01(rnorm(100))), 0, tolerance = 1e-5)
     expect_equal(max(scale01(rnorm(100, 100, 100))), 1, tolerance = 1e-5)
 })
-
-
